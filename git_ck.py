81414ae991d89876a4d13d7c14b06a917c6a1263
SimoParrella <moneparrella@libero.it>
2023-10-27 19:40:33 +0200

    metriche per intervallo
    manca ottimizzazione perchè sono lenti

--- git_ck.py
+++ git_ck.py
@@ -4,30 +4,36 @@ from git import Repo
 import repo_utils as ru
 import pandas as pd
 from multiprocessing import Pool
+import threading
 
-def ck_metrics_for_single_commit(commit_hash):
-    # Questo metodo estrae le metriche del commit scelto
-    repo_to_analyze = os.path.abspath('Repository')
-    ck_tool = os.path.abspath('ck.jar')
-    output_dir = os.path.abspath('output') 
+resource_lock = threading.Lock()
 
-    os.chdir(repo_to_analyze)
 
-    # Verifica se il commit esiste nella repository
-    try:
-        repo = Repo(repo_to_analyze)
-        repo.commit(commit_hash)
-    except:
-        print(f"Il commit con hash: {commit_hash} non è stato trovato nella repository.")
-        return
-    
-    # Effettua il checkout del commit
-    subprocess.call(['git', 'checkout', '-f', commit_hash])
-    os.chdir(os.path.dirname(ck_tool))
-    subprocess.call(['java', '-jar', 'ck.jar', repo_to_analyze, 'true', '0', 'true', f"{output_dir}/{commit_hash}"])
+# def ck_metrics_for_single_commit(commit_hash):
+#     # Questo metodo estrae le metriche del commit scelto
+#     repo_to_analyze = os.path.abspath('Repository')
+#     ck_tool = os.path.abspath('ck.jar')
+#     output_dir = os.path.abspath('output') 
+
+#     os.chdir(repo_to_analyze)
+#     subprocess.call(['git', 'checkout', '-f', commit_hash])
+#     os.chdir(os.path.dirname(ck_tool))
+#     subprocess.call(['java', '-jar', 'ck.jar', repo_to_analyze, 'true', '0', 'true', f"{output_dir}/{commit_hash}"])
     # ru.delete_garbage("class")
     # Non ritorna nulla ma crea il file csv con metriche per il commit richiesto
+    
 
+
+def ck_metrics_for_single_commit(commit_hash):
+    with resource_lock:
+        repo_to_analyze = os.path.abspath('Repository')
+        ck_tool = os.path.abspath('ck.jar')
+        output_dir = os.path.abspath('output') 
+    
+        os.chdir(repo_to_analyze)
+        subprocess.call(['git', 'checkout', '-f', commit_hash])
+        os.chdir(os.path.dirname(ck_tool))
+        subprocess.call(['java', '-jar', 'ck.jar', repo_to_analyze, 'true', '0', 'false', f"{output_dir}/{commit_hash}"])
 
 
 def commit_measure(measures, commit_hash):
@@ -132,7 +138,7 @@ def commit_for_yearConc(year):
 
 
 
-def commit_for_MonthYearConc(start_year, start_month, end_year, end_month):
+def commit_for_MonthYear(start_year, start_month, end_year, end_month):
     repo_to_analyze = os.path.abspath('Repository')
     repo = Repo(repo_to_analyze)
     
@@ -142,7 +148,7 @@ def commit_for_MonthYearConc(start_year, start_month, end_year, end_month):
         if start_year <= commit_date.year <= end_year and start_month <= commit_date.month <= end_month:
             selected_commits.append(commit)
 
-    # Create a Pool of worker processes
-    with Pool() as pool:
-        pool.map(process_commit, selected_commits)
-    ru.delete_garbage("class")
+    for commit in selected_commits:
+            commit_hash = commit.hexsha
+            ck_metrics_for_single_commit(commit_hash)
+            ru.delete_garbage("class")
