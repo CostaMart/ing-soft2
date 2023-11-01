3518dab2d539f804837ad8677d243bde0dacb0e1
SimoParrella <moneparrella@libero.it>
2023-11-01 15:02:32 +0100

    Inserimento ultime metriche di processo
    Ho aggiunto i metodi che mancavano alle metriche di processo, modificato qualcosa per quanto riguarda l'analisi per release, aggiunto una classe che calcola le metriche di processo di tutto un commit inserito quello per il commit principale e documentato il resto

--- repo_utils.py
+++ repo_utils.py
@@ -3,19 +3,14 @@ import pandas as pd
 import json
 from pydriller import Repository
 import subprocess
-import shutil
-import glob
-import re
-import git
 
 setting = open("settings.json")
 settings = json.load(setting)
 remote_repo = settings['repo']
-folder = "repository"
 
 
 
-def check_repo():
+def check_repo(folder = "repository"):
     """Metodo che controlla se il progetto da analizzare è presente"""
     if os.path.exists(folder):
         content = os.listdir(folder)
@@ -26,17 +21,40 @@ def check_repo():
 
 
 
-def check_folder():
+def check_folder(folder = "output"):
     """Metodo che controlla se la cartella di output è presente"""
-    if not os.path.exists("output"):
-        path = os.path.join("output")
+    if not os.path.exists(folder):
+        path = os.path.join(folder)
         os.mkdir(path)
 
 
 
-def clone_repo():
+def clone_repo(folder = "repository"):
     """Metodo che effettua il clone di un repository target"""
     subprocess.call(['git', 'clone', remote_repo, folder])
+    
+
+
+def clone_git_repository_with_tag(tag):
+    """Questo metodo clona una repository con il tag specificato"""
+    try:
+        subprocess.run(['git', 'clone', '-b', tag, remote_repo], check=True)
+        print(f"Repository clonata con successo utilizzando il tag {tag}.")
+    except subprocess.CalledProcessError as e:
+        print(f"Errore durante il clone: {e.stderr}")
+
+
+
+def get_commit_hash_for_tag(tag, folder = "repository"):
+    try:
+        repository_path=os.path.abspath(folder)
+        # Esegui il comando 'git rev-list' per ottenere l'hash del commit associato al tag
+        result = subprocess.run(['git', 'rev-list', '-n', '1', tag], cwd=repository_path, capture_output=True, text=True, check=True)
+        commit_hash = result.stdout.strip()
+        return commit_hash
+    except subprocess.CalledProcessError as e:
+        print(f"Errore nell'ottenere l'hash del commit per il tag {tag}: {e.stderr}")
+        return None
     
 
 
@@ -46,7 +64,7 @@ def print_current_branch(repository):
 
 
 
-def repo_to_use():
+def repo_to_use(folder = "repository"):
     """Metodo che restituisce un oggetto Repository"""
     return Repository(folder) 
 
@@ -80,11 +98,11 @@ def dataCommitLink(rep):
 
 
 
-def delete_garbage(keep, output=None):
+def delete_garbage(keep, output=None, folder = "output"):
 
     """Elimina i file non utilizzabili creati con le metriche della classe"""
     if(output is None):
-        output_dir = os.path.abspath("output")
+        output_dir = os.path.abspath(folder)
     else:
         output_dir = os.path.abspath("output")+"\\"+str(output)
     for filename in os.listdir(output_dir):
@@ -93,9 +111,9 @@ def delete_garbage(keep, output=None):
 
 
 
-def trova_file_classe(classe_filename):
+def trova_file_classe(classe_filename, folder = "repository"):
     """ Questo metodo trova una classe in una repository e ne ritorna il path assoluto """
-    repository_path = os.path.abspath("Repository")
+    repository_path = os.path.abspath(folder)
     for root, dirs, files in os.walk(repository_path):
         if classe_filename in files:
             return os.path.join(root, classe_filename)
@@ -114,3 +132,64 @@ def cerca_file_java(cartella_name):
                 risultati.append(file)
 
     return risultati
+
+
+
+def get_git_tags(folder = "repository"):
+    """Questo metodo restituisce i tag di una repository"""
+    repo_path = os.path.abspath(folder)+"\\.git" 
+    try:
+        result = subprocess.run(['git', '--git-dir', repo_path, 'tag'], capture_output=True, text=True)
+        
+        if result.returncode == 0:
+            # Il comando eseguito
+            output = result.stdout
+            tags = output.split('\n')
+            return [tag for tag in tags if tag]  # Rimuovi eventuali tag vuoti
+        else:
+            print("Errore nell'esecuzione del comando 'git tag':", result.stderr)
+            return []
+    except subprocess.CalledProcessError as e:
+        print("Errore nell'esecuzione del comando 'git tag':", e.stderr)
+        return []
+    
+
+
+def get_commit_date(commit_hash, folder = "repository"):
+    """Questo metodo restituisce la data del commit richiesto"""
+    try:
+        result = subprocess.run(['git', 'show', '--format=%aI', '-s', commit_hash], cwd=os.path.abspath(folder), capture_output=True, text=True, check=True)
+        commit_date = result.stdout.strip()
+        return commit_date
+    except subprocess.CalledProcessError as e:
+        print(f"Errore nell'ottenere la data del commit {commit_hash}: {e.stderr}")
+        return None
+
+
+
+def get_git_tags_commit(folder = "repository"):
+    """Questo metodo ottiene la lista dei tag e li inserisce in un dataframe con il commit specifico che ha creato il tag e la data del commit"""
+    tags = get_git_tags(folder)
+    
+    data = []
+    
+    for tag in tags:
+        commit_hash = get_commit_hash_for_tag(tag, folder)
+        
+        if commit_hash:
+            commit_date = get_commit_date(commit_hash, folder)
+            data.append({'Tag': tag, 'Commit Hash': commit_hash, 'Commit Date': commit_date})
+    
+    df = pd.DataFrame(data)
+    
+    return df
+
+
+
+def checkout_commit(commit_hash, folder = "repository"):
+    """Questo metodo effettua il checkout a un commit specifico"""
+    try:
+        subprocess.run(['git', 'checkout', commit_hash], cwd=os.path.abspath(folder), check=True)
+        print(f"Checkout effettuato con successo al commit {commit_hash}.")
+    except subprocess.CalledProcessError as e:
+        print(f"Errore durante il checkout al commit {commit_hash}: {e.stderr}")
