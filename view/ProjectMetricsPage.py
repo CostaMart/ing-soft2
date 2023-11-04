d490ebd3336c537997db120dba6389611ca5bef5
CostaMart <Costa.martignetti@gmail.com>
2023-11-04 19:29:23 +0100

    aggiustata una piccola cosa

--- view/ProjectMetricsPage.py
+++ view/ProjectMetricsPage.py
@@ -107,4 +107,4 @@ class ProjectMetricsPage(ctk.CTkScrollableFrame):
         self.optionMenu = ctk.CTkOptionMenu(self.optionFrame, values=["calss1", "class2"])
         self.optionMenu.set("calss1")
         self.optionMenu.pack(padx = 10, pady = 5)
-        self.button.pack()
+       
