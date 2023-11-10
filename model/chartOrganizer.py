ad012ad79f6c8000a0f5d3d9aaa62a36e5786e82
SimoParrella <moneparrella@libero.it>
2023-11-10 09:32:39 +0100

    Aggiunta nuovi metodi per grafici
    Guardare la descrizione di ogni metodo di chartOrganizer ci sono scritti i grafici da fare

--- model/chartOrganizer.py
+++ model/chartOrganizer.py
@@ -2,19 +2,27 @@ import pandas as pd
 
 def revision_number(df):
     """Da utilizzare con grafico a barre X = Nome della Classe, Y = Numero di Revisioni"""
-    return df[["Nome della Classe", "Numero di Revisioni"]]
+    return df[["Data del Commit", "Numero di Revisioni"]]
 
 def loc_number(df):
-    """Da utilizzare con grafico a torta Titolo = LOC 'Nome della Classe' e ogni spicchio è una di queste cose"""
-    return df[["Nome della Classe", "Linee di Codice", "Linee Vuote", "Commenti"]]
+    """Grafico a linee: L'indice è la data del commit le linee sono gli altri attributi"""
+    return df[["Data del Commit", "Linee di Codice", "Linee Vuote", "Commenti"]]
 
 def authors(df):
-    """Da utilizzare con grafico a torta Titolo = Autori 'Nome della Classe' e ogni spicchio è un autore """
-    return df[["Nome della Classe", "Autori"]]
+    """Grafico a dispersione che dovrebbe rappresnetare la presenza di quell'autore nel tempo(quindi nel commit)"""
+    return df[["Data del Commit", "Autori"]]
 
 def weeks(df):
-    """Da utilizzare con grafico a barre X = Nome della Classe, Y = Settimane File"""
-    return df[["Nome della Classe", "Settimane file"]]
+    """Da utilizzare con grafico a barre X = Data del Commit, Y = Settimane File"""
+    return df[["Data del Commit", "Settimane file"]]
+
+def codeC(df):
+    """Da utilizzare grafico a barre per quantità quindi X= Data del Commit, Y = Code Churn"""
+    return df[["Data del Commit", "Code churn con hash1"]]
+
+def bugfix(df):
+    """Da utilizzare con grafico a barre vicino alle metriche di ck però X = Data del Commit, Y = bugfix commit"""
+    return df[["Data del Commit", "bugfix commit"]]
 
 def wmc(df):
     """Da utilizzare con grafico a linea X = Data Del Commit Y = wmc"""
