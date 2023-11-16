f81c1300aaac7042e8418bc1ac9c59d79889af74
SimoParrella <moneparrella@libero.it>
2023-11-16 11:07:24 +0100

    bugfix
    risolto il bug per cui quando facevi un'analisi fino a un certo commit con le analisi dopo non trovavi più i commit dopo l'ultimo commit analizzato

--- model/spMetrics.py
+++ model/spMetrics.py
@@ -2,12 +2,14 @@ import model.process_metrics as pm
 import model.repo_utils as ru
 import pandas as pd
 import model.git_ck as ck
+import subprocess
+import os
 
 
 def generate_process_metrics(nome_classe, commits_dict, folder = "repository"):
     """Genera le metriche di processo, è possibile specificare il commit da analizzare e il folder dove è conservata la repository"""
     """Questo metodo dovrà essere utilizzato per fare tutte le analisi disponibili con le metriche di processo, a parte i code churn"""
-    # df_filtrato = ru.estrai_parametri(json_list)
+    latest_commit_hash = subprocess.check_output(["git", "rev-parse", "HEAD"], cwd=os.path.abspath(folder), text=True).strip()
     df_filtrato = pd.DataFrame(list(commits_dict.items()), columns=['Commit Hash', 'Data del Commit'])
     df_filtrato['Data del Commit'] = df_filtrato['Data del Commit'].apply(lambda x: x['date'])
     hash1 = df_filtrato["Commit Hash"][0]
@@ -34,6 +36,7 @@ def generate_process_metrics(nome_classe, commits_dict, folder = "repository"):
         df_finale = pd.concat([df_finale, temp_df], ignore_index=True)
         # df_finale['Data del Commit'] = pd.to_datetime(df_finale['Data del Commit'])
         # df_finale['Data del Commit'] = df_finale['Data del Commit'].dt.strftime('%Y-%m-%d %H:%M:%S')
+    ru.checkout_commit(latest_commit_hash)
     return df_finale
 
 
@@ -44,13 +47,15 @@ def generate_metrics_ck(commits_dict, folder = "repository", measures =["cbo", "
     """Genera le metriche ad oggetti , questo metodo prende la release da cui si vuole partire(opzionale), trova i commit e li inserisce in un dataframe"""
     """Poi li filtra dal commit dopo la release precedente, alla release scelta e ritorna il primo dataframe filtrato,"""
     """Una volta richiamato il metodo, se gli si passa il dataframe e i due hash per cui si vuole avere l'intervallo di analisi, filtra di nuovo il df e analizza"""
-
+    latest_commit_hash = subprocess.check_output(["git", "rev-parse", "HEAD"], cwd=os.path.abspath(folder), text=True).strip()
     df_filtrato = pd.DataFrame(list(commits_dict.items()), columns=['Commit Hash', 'Data del Commit'])
     df_filtrato['Data del Commit'] = df_filtrato['Data del Commit'].apply(lambda x: x['date'])
     commit=ck.commit_measure_interval(measures, df_filtrato, folder)
     if(commit.empty): 
         print("Non ci sono commit disponibili")
+        ru.checkout_commit(latest_commit_hash)
         return 0
+    ru.checkout_commit(latest_commit_hash)
     return commit
 
 
