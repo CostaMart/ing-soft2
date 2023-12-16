f422405e1bc9ef513c683045369c8afa7f49205e
Angelo Romano <102727644+Angelorom@users.noreply.github.com>
2023-12-16 17:50:41 +0100

    gestione delle dipendenze nella pipeline

--- start_app.py
+++ start_app.py
@@ -7,27 +7,19 @@ from view.StartupErrorPage import StartupErrorPage
 
 if __name__ == "__main__":
     """ entry point dell'applicazione """
-    
 
     SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
     sys.path.append(os.path.dirname(SCRIPT_DIR))
 
-  
-
     start = StartAppController()
-
 
     status = start.startComputationEndpoint()
     time.sleep(5)
 
-
     isInstalled, version = start.isGitInstalled()
     response = start.isComputeEndpointActive()
 
-
     if isInstalled:
-        IngSoftApp(gitv = version, endpointStatus = status)
-    else:   
+        IngSoftApp(gitv=version, endpointStatus=status)
+    else:
         StartupErrorPage()
-    
-
