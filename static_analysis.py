eb3b67dfabda4d07b7a3bd71eac542acb823a425
Angelo Romano <102727644+Angelorom@users.noreply.github.com>
2023-12-20 12:36:04 +0100

    modifiche pipe

--- static_analysis.py
+++ static_analysis.py
@@ -1,3 +1,4 @@
+import re
 import subprocess
 
 def run_pylint(file_name, output_file):
@@ -13,6 +14,13 @@ def run_pylint(file_name, output_file):
         with open(output_file, "a") as report_file:
             report_file.write(f"Pylint output for {file_name}:\n{result.stdout}\n\n")
 
+        # Estrai il punteggio di Pylint dall'output
+        match = re.search(r"Your code has been rated at ([\d\.]+)/10", result.stdout)
+        if match:
+            return float(match.group(1))
+
+    return 0.0  # Restituisci 0 se non è stato possibile eseguire Pylint o estrarre il punteggio
+
 def main():
     # Specifica il nome del file globale per tutti i report
     global_output_file = "pylint_report.txt"
@@ -23,12 +31,18 @@ def main():
         file_names = [line.strip() for line in file]
 
         # Analizza ciascun file con Pylint e aggiungi l'output al file globale
+        scores = []
         for file_name in file_names:
             # Unisci il percorso del progetto e il nome del file
             full_path = f"{file_name}.py"
 
             # Esegui Pylint e aggiungi l'output al file globale
-            run_pylint(full_path, global_output_file)
+            score = run_pylint(full_path, global_output_file)
+            scores.append(score)
+
+        # Calcola e restituisci il punteggio medio
+        average_score = sum(scores) / len(scores) if scores else 0.0
+        print(average_score)
 
 if __name__ == "__main__":
     main()
